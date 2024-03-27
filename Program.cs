2de4fc241a89eb410419682f141bd2f740f40d20
archistico <info@archistico.com>
2024-03-27 16:27:35 +0100

    add image to sqlite db

--- Program.cs
+++ Program.cs
@@ -16,6 +16,10 @@ using System.Data.SQLite;
 // dotnet add package System.Data.SQLite --version 1.0.118
 // https://www.codeguru.com/dotnet/using-sqlite-in-a-c-application/
 // dotnet run --property WarningLevel=0
+// Su VSCode extension: sqlite by alexcvzz
+// sudo apt install sqlite3
+
+// TODO: Aggiungere HASH?
 
 namespace Foto_CreaDB2
 {
@@ -24,11 +28,132 @@ namespace Foto_CreaDB2
         static void Main(string[] args)
         {
             string[] paths = { @"/home/emilie/Immagini" };
-            RecursiveFileProcessor.Cerca(paths);
+            //RecursiveFileProcessor.Cerca(paths);
+
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+            CreateTable(sqlite_conn);
+
+            Foto f = new Foto();
+            f.nomefile = "nomefile.jpg";
+            f.data = DateTime.Now.ToString();
+            f.cartella = "cartella";
+
+            InsertData(sqlite_conn, f);
+            //ReadData(sqlite_conn);
+
+            sqlite_conn.Close();
 
             Console.Write($"Fine... ");
             Console.ReadKey();
-        }                
+        }
+
+        static SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=foto.db;Version=3;New=True;Compress=True;");
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+
+        static void CreateTable(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE FOTO ("
+                + "  ID INTEGER PRIMARY KEY AUTOINCREMENT"
+                + ", NOMEFILE TEXT NOT NULL"
+                + ", CARTELLA TEXT NOT NULL"
+                + ", DATA TEXT"
+                + ", MIME TEXT"
+                + ", DIMENSIONE INTEGER"
+                + ", LARGHEZZA INTEGER"
+                + ", ALTEZZA INTEGER"
+                + ", MARCA TEXT"
+                + ", MODELLO TEXT"
+                + ", ESPOSIZIONE TEXT"
+                + ", APERTURA TEXT"
+                + ", ISO TEXT"
+                + ", COMPENSAZIONE TEXT"
+                + ", ZOOM TEXT"
+                + ")"
+                ;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        static void InsertData(SQLiteConnection conn, Foto foto)
+        {
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO FOTO ("
+                + "  NOMEFILE"
+                + ", CARTELLA"
+                + ", DATA"
+                + ", MIME"
+                + ", DIMENSIONE"
+                + ", LARGHEZZA"
+                + ", ALTEZZA"
+                + ", MARCA"
+                + ", MODELLO"
+                + ", ESPOSIZIONE"
+                + ", APERTURA"
+                + ", ISO"
+                + ", COMPENSAZIONE"
+                + ", ZOOM"
+                + ") " 
+                + " VALUES (@nomefile, @cartella, @data, @mime, @dimensione"
+                + ", @larghezza, @altezza, @marca, @modello, @esposizione"
+                + ", @apertura, @iso, @compensazione, @zoom);";
+
+            sqlite_cmd.Parameters.AddWithValue("nomefile", foto.nomefile);
+            sqlite_cmd.Parameters.AddWithValue("cartella", foto.cartella);
+            sqlite_cmd.Parameters.AddWithValue("data", foto.data);
+            sqlite_cmd.Parameters.AddWithValue("mime", foto.mime);
+            sqlite_cmd.Parameters.AddWithValue("dimensione", foto.dimensione);
+            sqlite_cmd.Parameters.AddWithValue("larghezza", foto.larghezza);
+            sqlite_cmd.Parameters.AddWithValue("altezza", foto.altezza);
+            sqlite_cmd.Parameters.AddWithValue("marca", foto.marca);
+            sqlite_cmd.Parameters.AddWithValue("modello", foto.modello);
+            sqlite_cmd.Parameters.AddWithValue("esposizione", foto.esposizione);
+            sqlite_cmd.Parameters.AddWithValue("apertura", foto.apertura);
+            sqlite_cmd.Parameters.AddWithValue("iso", foto.iso);
+            sqlite_cmd.Parameters.AddWithValue("compensazione", foto.compensazione);
+            sqlite_cmd.Parameters.AddWithValue("zoom", foto.zoom);
+    
+            try {
+                sqlite_cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) {
+                throw new Exception(ex.Message);
+            }    
+            
+        }
+
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM SampleTable";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+        }
     }
 
     public class RecursiveFileProcessor
@@ -157,5 +282,23 @@ namespace Foto_CreaDB2
 
             return ris;
         }
+    }
+
+    public class Foto
+    {
+        public string nomefile { get; set; } = ""; 
+        public string cartella { get; set; } = ""; 
+        public string data { get; set; } = ""; 
+        public string mime { get; set; } = ""; 
+        public int dimensione { get; set; } = 0; 
+        public int larghezza { get; set; } = 0; 
+        public int altezza { get; set; } = 0; 
+        public string marca { get; set; } = ""; 
+        public string modello { get; set; } = ""; 
+        public string esposizione { get; set; } = ""; 
+        public string apertura { get; set; } = ""; 
+        public string iso { get; set; } = ""; 
+        public string compensazione { get; set; } = ""; 
+        public string zoom { get; set; } = ""; 
     }
 }
